56482985c2d13f06df33d58bbb2876a6ed2b9f83
Chester Li <zicheng.li@yale.edu>
2024-08-07 18:54:22 -0400

    Update Toy_driver_script.sh

--- test/Toy_driver_script.sh
+++ test/Toy_driver_script.sh
@@ -9,31 +9,30 @@ upper=0.9
 lower=0.1
 
 ${tool_dir}/GSEXP \
-unique \
+discfreq \
 -i ${dir}/Toy.vcf \
 -o ${dir}/Toy_fixed_ref.txt \
 -p ${dir}/Toy_pop_label.txt \
 -t 2 \
 --flip \
---effsample ${eff_sample} \
---mind 10 \
---mins 3 \
---popnum 1 \
---p1l ${upper} --p1u 1.0 --p2l 0.0 --p2u ${lower} 
+--min-total-sample ${eff_sample} \
+--min-depth 10 \
+--min-sample 3 \
+--group-num 1 \
+--tar-lower ${upper} --tar-upper 1.0 --ref-lower 0.0 --ref-upper ${lower} 
 
 ${tool_dir}/GSEXP \
-lhdisc \
+disclh \
 -i ${dir}/Toy.vcf \
 -o ${dir}/Toy_ml_ref.txt \
 -p ${dir}/Toy_pop_label.txt \
 -t 2 \
 --flip \
 --lh-thres -0.0031 \
---effsample ${eff_sample} \
---mind 10 \
---mins 3 \
---popnum 1 \
---p1l ${upper} --p1u 1.0 --p2l 0.0 --p2u ${lower} 
+--min-total-sample ${eff_sample} \
+--min-depth 10 \
+--min-sample 3 \
+--group-num 1 \
 
 ${tool_dir}/Postprocessing ${dir} Toy_fixed_ref
 ${tool_dir}/Postprocessing ${dir} Toy_ml_ref
